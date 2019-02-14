96609662b7edb6fba984444d7c6d0e759c93b5de
hdson07 <43102442+hdson07@users.noreply.github.com>
2019-02-14 15:53:04 +0900

    add vine

--- Structure/Structure/ViewController/Category/Category_CollectionViewCell.swift
+++ Structure/Structure/ViewController/Category/Category_CollectionViewCell.swift
@@ -46,8 +46,6 @@ class Category_CollectionViewCell: UICollectionViewCell {
     func DeleteCustom(customMode : Bool){
         if customMode == true && categoryTitleLabel.text != "추가하기"{
             let deleteButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
-            self.backgroundColor = #colorLiteral(red: 0.5129458308, green: 0.7746969461, blue: 0.6973600984, alpha: 1)
-            self.addSubview(favoritButton!)
             self.backgroundColor = #colorLiteral(red: 0.9581267238, green: 0.7396259904, blue: 0.4966012239, alpha: 1)
             self.addSubview(favoritButton)
             deleteButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
@@ -62,7 +60,6 @@ class Category_CollectionViewCell: UICollectionViewCell {
         }else{
             self.backgroundColor = #colorLiteral(red: 0.9581267238, green: 0.7396259904, blue: 0.4966012239, alpha: 1)
             self.addSubview(favoritButton)
-            deleteButton.removeFromSuperview()
         }
         
     }
